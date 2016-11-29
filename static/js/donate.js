570565353659fa3141d52876d907650f7bdfd107
aapinkert <aapinkert@gmail.com>
2016-11-29 14:15:54 -0600

    Adding Inital Files

---
+++ static/js/donate.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#confimationBtn').click(function() {
+        event.preventDefault();
+        if(!$("#checkboxConfirm").prop('checked')){
+            bootbox.alert({
+                message: "Please confirm the terms and conditions. ",
+                callback: function () {
+                    location.reload();
+                }
+            })
+        }
+        else{
+            window.location.href = "donate-confirm.html"
+        }
+    });
+});
